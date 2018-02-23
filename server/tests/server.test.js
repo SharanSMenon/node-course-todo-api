5ac9fa15888dc874e4176a9d2ca72eb31a62a74f
Sharan Sajiv Menon <sharansajivmenon@gmail.com>
2018-02-23 16:45:12 -0500

    Added Queries playground

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,15 @@ const request = require('supertest');
 //Local imports
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const todos = [{
+    text:'test todo 1'
+},{
+    text:'test todo 2'
+}];
 beforeEach((done) => {
-    Todo.remove({}).then(() => done())
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done())
 });
 //Main code and test cases
 describe('POST /todos', () => {
@@ -23,7 +30,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(1)
+                    expect(todos.length).toBe(3)
                     expect(todos[0].text).toBe(text);
                     done();
             })
@@ -40,7 +47,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             });
